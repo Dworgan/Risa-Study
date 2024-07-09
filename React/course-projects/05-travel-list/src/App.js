ea5fb34124d9f1c227be3ce5876fd78a3867d6af
Remi <r.newton84@gmail.com>
2024-07-09 16:51:57 +0200

    React Projects

---
+++ React/course-projects/05-travel-list/src/App.js
@@ -0,0 +1,71 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Shoes", quantity: 2, packed: false },
+  { id: 4, description: "Toothbrush", quantity: 4, packed: false },
+  { id: 5, description: "Toothpaste", quantity: 2, packed: true },
+  { id: 5, description: "Sunglasses", quantity: 4, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((nmbr) => (
+          <option value={nmbr} key={nmbr}>
+            {nmbr}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item.."></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item, key }) {
+  return (
+    <li>
+      <span className={item.packed ? "text-strikethrough" : ""}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X(%)</em>
+    </footer>
+  );
+}
