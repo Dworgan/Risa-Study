78d1678ab0c811ddd55bfd9fa25271d403a34e1a
Remi <r.newton84@gmail.com>
2024-07-09 17:12:38 +0200

    C# Course

---
+++ C# Masterclass/Excersices/ToDoList/ToDoList/Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello!");
+int _result;
+int _firstInput;
+int _lastInput;
+string _choice;
+
+Console.WriteLine("Input the first number:");
+_firstInput = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the Second number:");
+_lastInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to  do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubstract numbers");
+Console.WriteLine("[M]ultiply numbers");
+
+_choice = Console.ReadLine();
+
+if(_choice == "a" || _choice == "A" )
+{
+    Console.WriteLine(_firstInput + " + " + _lastInput + " = " + (_firstInput + _lastInput));
+}
+else if (_choice == "s" || _choice == "S")
+{
+    Console.WriteLine(_firstInput + " - " + _lastInput + " = " + (_firstInput + _lastInput));
+}
+else if (_choice == "m" || _choice == "M")
+{
+    Console.WriteLine(_firstInput + " * " + _lastInput + " = " + (_firstInput + _lastInput));
+}
+else
+{
+    Console.WriteLine("Wrong type selected");
+}
+
+int a = 4, b= 5, c = 6;
+Console.WriteLine(
+        $"First is: {a}, second is {b}");
+
+Console.WriteLine("Press any key to Exit");
+
+Console.ReadKey();
---
+++ C# Masterclass/Excersices/ToDoList/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] tNumber = new int[] { 10, -2, -12, 3, 5 };
+int _nonPositiveNmbrs;
+List<int> _positiveNmbrList = GetOnlyPositiveNumbers(tNumber, out _nonPositiveNmbrs);
+foreach(int nmbr in _positiveNmbrList)
+{
+    Console.WriteLine(nmbr);
+}
+Console.WriteLine("Total of negative numbers = "+ _nonPositiveNmbrs);
+
+Console.ReadKey();
+
+List<int> GetOnlyPositiveNumbers(int[] tNumber, out int nonPositiveNmbrs)
+{
+    nonPositiveNmbrs = 0;
+    List<int> result = new List<int>();
+    for (int i = 0; i < tNumber.Length; i++)
+    {
+        if (tNumber[i] > 0)
+        {
+            result.Add(tNumber[i]);
+        }
+        else
+        {
+            nonPositiveNmbrs++;
+        }
+    }
+    return result;
+}
